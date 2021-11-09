dd90becad8394049b67a94f5988627ff9e3bd805
lgvcelulares <projetophpa3@gmail.com>
2021-11-08 22:52:42 -0300

    Update script_primario.sql

--- script_primario.sql
+++ script_primario.sql
@@ -1,15 +1,19 @@
 create database if not exists phpa3;
+
 use phpa3;
+
 create table if not exists funcionarios(
 id_fun int primary key auto_increment,
 nome_fun varchar(45) not null,
 status_fun enum('ativo', 'inativo'),
 matricula varchar(45)
 );
+
 create table if not exists vendas(
 id_vendas int primary key auto_increment,
 data_venda varchar(45) not null
 );
+
 create table if not exists produtos(
 id_prod int primary key auto_increment,
 nome_prod varchar(45) not null,
