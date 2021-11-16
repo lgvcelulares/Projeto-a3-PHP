c79fb148cc510f66e6ef47d86f7f8c95e36911ca
lgvcelulares <projetophpa3@gmail.com>
2021-11-16 19:35:27 -0300

    importação bootstrap e criação do menu vertical

--- index.php
+++ index.php
@@ -4,9 +4,46 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/bootstrap.css">
+    <link rel="stylesheet" href="CSS/meuCSS.css">
     <title>lgvcelulares</title>
 </head>
 <body>
-    
+<nav class="navbar navbar-expand-lg secondary-bg-color" id="bottom-navbar-container">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#bottom-navbar"
+                aria-expanded="true" aria-label="Navigation" id="expande">
+                <i class="bi bi-list"></i>
+            </button>
+            <ul class="navbar-nav mb-2 mb-lg-0 collapse navbar-collapse" id="bottom-navbar">
+                <li>
+                    <a href="index.html" class="nav-link">Home</a>
+                </li>
+                <li>
+                    <a href="politica.html" class="nav-link">Política</a>
+                </li>
+                <li>
+                    <a href="espotes.html" class="nav-link">Espotes</a>
+                </li>
+                <li>
+                    <a href="economia.html" class="nav-link">Economia</a>
+                </li>
+                <li>
+                    <a href="saude.html" class="nav-link">Saúde</a>
+                </li>
+                <li>
+                    <a href="educacao.html" class="nav-link">Educação</a>
+                </li>
+                <li>
+                    <div id="pesquisa-materia">
+                        <form class="d-flex" id="search-form">
+                            <input type="text" placeholder="Buque por uma matéria ou topico" class="" id="inp_pesquisa" aria-label="Search">
+                            <button class="btn btn-danger" id="btnPesquisar" type="submit">Pesquisar</button>
+                        </form>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </body>
 </html>
