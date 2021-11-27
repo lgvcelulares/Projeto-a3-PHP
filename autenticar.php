46ac7751c7a9aebd0ef1552a355e7d1686d44134
lgvcelulares <projetophpa3@gmail.com>
2021-11-27 11:43:03 -0300

    Update autenticar.php

--- autenticar.php
+++ autenticar.php
@@ -3,12 +3,12 @@
             echo $_SESSION["login_fun"], $_SESSION["login_cliente"];
     } else {
         echo "<script> 
-				alert ('Você não está logado!!!') 
+				alert ('Você não está logado!!!')
+        location.href = ('index.php') 
 			  </script>";
 			
 		echo "<script> 
 				location.href = ('index.php') 
 			  </script>";
     }
-    
 ?>
